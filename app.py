414aa0076b9171247443d6466d8c17148df8aa82
Parviz Mammadov <65316731+Parviz1989@users.noreply.github.com>
2025-05-17 13:56:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Parviz! This is your Flask app deployed with CI/CD 🚀"
+    return "Hello, Parviz! This is your Flask app deployed with CI/CD - Azerbaijan 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
